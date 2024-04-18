6eeb0567c810530f7a8230ec3b277de6785201df
unknown <porteromatias@gmail.com>
2024-04-18 14:50:09 -0300

    for para varias personas java

--- 5°10°/Laboratorio de POO/Portero/Java/Portero/src/Main.java
+++ 5°10°/Laboratorio de POO/Portero/Java/Portero/src/Main.java
@@ -1,11 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    static Persona p1, p2;
+    static Persona p1;
 
     public static void main(String[] args) {
-        p1 = new Persona("Matute", "18/04/2007");
-        System.out.println(p1.nombre + " nació el " + p1.fecha_nac + " y tiene " + p1.edad() + " años.");
+        int x;
+        List<Persona> personas = new ArrayList<>();
 
-        p2 = new Persona("Juan", "15/05/1980");
-        System.out.println(p2.nombre + " nació el " + p2.fecha_nac + " y tiene " + p2.edad() + " años.");
-    }
+        for (int i = 0; i < 10; i++) {
+           p1 = new Persona("Matute", "18/04/2007");
+            personas.add(p1);
+
+           p1 = new Persona("Mazza", "20/04/1997");
+            personas.add(p1);
+
+            p1 = new Persona("Uni", "18/11/2010");
+            personas.add(p1);
+
+            p1 = new Persona("Santutu", "11/09/2001");
+            personas.add(p1);
+        }
+
+        System.out.println(personas.get(3).nombre + " nació el " + personas.get(3).fecha_nac + " y tiene " + personas.get(3).edad() + " años.");    }
 }
