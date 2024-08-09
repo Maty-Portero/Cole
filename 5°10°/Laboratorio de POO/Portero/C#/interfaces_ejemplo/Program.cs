77cad4b9e5f7a7a6a8103b892440d49d0c6fde6d
unknown <porteromatias@gmail.com>
2024-08-09 14:53:40 -0300

    ejer 1 obligatorio listo

---
+++ 5°10°/Laboratorio de POO/Portero/C#/interfaces_ejemplo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces_ejemplo
+{
+
+    interface IAves
+    {
+        string Name { get; }
+        void comer(int cantidad);
+    }
+
+    interface IVolar
+    {
+        string Name { get; }
+        void volar(int altura);
+    }
+
+    class Pajaro : IAves, IVolar
+    {
+        public int cantidadComida = 0;
+        public void comer(int cantidad)
+        {
+
+        }
+        public void volar(int altura)
+        {
+
+        }
+        public string Name { get; }
+    }
+
+    class Ñandu : IAves
+    {
+        public int cantidadComida = 0;
+        public void comer(int cantidad)
+        {
+            cantidadComida += cantidad;
+        }
+        public string Name { get; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
