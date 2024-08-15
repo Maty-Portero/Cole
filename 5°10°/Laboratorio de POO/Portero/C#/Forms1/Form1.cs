b9fdb378334631ded4d24cf86e2af9def7e04f04
unknown <porteromatias@gmail.com>
2024-08-15 17:49:05 -0300

    hago algunos cambios

---
+++ 5°10°/Laboratorio de POO/Portero/C#/Forms1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {            
+            if (MessageBox.Show("Desea salir?", "Cerrar", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Left = 500;
+        }
+    }
+}
