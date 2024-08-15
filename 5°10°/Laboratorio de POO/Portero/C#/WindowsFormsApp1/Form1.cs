75c8d822b70d31db5b5986f5398b83669d71020c
Maty-Portero <porteromatias@gmail.com>
2024-08-15 19:58:57 -0300

    agrego un form motivacional

---
+++ 5°10°/Laboratorio de POO/Portero/C#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("muy bien amigo, aprobaste", "Aprobacion", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("no podes reprobar, en serio deseas rendirte?", "Rendicion", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            e.Cancel = true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("como vas a reporbar, alto fraca, volve a intentarlo", "Reprobacion", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
