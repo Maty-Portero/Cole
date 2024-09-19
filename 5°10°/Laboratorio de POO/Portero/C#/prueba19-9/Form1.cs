86709eca7797a237a3deeca34877f1b01777604f
adrianAlayo <axelalayo8@gmail.com>
2024-09-19 17:50:57 -0300

    agrego cosas de el 15

---
+++ 5°10°/Laboratorio de POO/Portero/C#/prueba19-9/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba19_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex % 2 == 0)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            for (int i = 0; i < 5; i++)
+            {
+                listBox1.Items.Add(listBox2.Items);
+                listBox2.Items.Clear();                                
+            }
+
+        }
+    }
+}
